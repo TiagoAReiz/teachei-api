2a6a9e01fe9b583692f267740a1f495a632fa965
Tiago Reiz <tiagoreizzzz@gmail.com>
2025-12-24 21:32:36 -0300

    inittial commit

---
+++ src/main/java/Reiz/TeAchei/TeAcheiApplication.java
@@ -0,0 +1,13 @@
+package Reiz.TeAchei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeAcheiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeAcheiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Reiz/TeAchei/TeAcheiApplicationTests.java
@@ -0,0 +1,13 @@
+package Reiz.TeAchei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeAcheiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
